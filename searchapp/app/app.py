96990c988e36da04da25195ea4cad9e5de44dfde
Luc Tran <luctv@relaxgame.vn>
2019-09-11 04:45:53 +0700

    Implement products aggregating API acording to prices.

--- searchapp/app/app.py
+++ searchapp/app/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 from searchapp.data import all_products
-from searchapp.app.search import search
+from searchapp.app.search import search, search_for_price_range
 
 app = Flask(__name__)
 
@@ -22,13 +22,25 @@ def search_single_product():
 
     Return the top 60 results.
     """
-    query = request.args.get('search')
     num_results = 60
-    products_by_category = [(query, search(query, num_results))]
+    products_by_category = []
+
+    name_or_description = request.args.get('search')
+    if name_or_description != None:
+        products_by_category = [(name_or_description, search(name_or_description, num_results))]
+    elif request.args.get('min_price') != None or request.args.get('max_price') != None:
+        min_price = request.args.get('min_price')
+        if min_price == None or min_price.isdigit() == False:
+            min_price = 0
+        max_price = request.args.get('max_price')
+        if max_price == None or max_price.isdigit() == False:
+            max_price = 0
+        products_by_category = [("Price in [{}, {}] VND".format(min_price, max_price),
+                                search_for_price_range(min_price, max_price, num_results))]
+
     return render_template(
         'index.html',
-        products_by_category=products_by_category,
-        search_term=query,
+        products_by_category=products_by_category
     )
 
 
--- searchapp/app/search.py
+++ searchapp/app/search.py
@@ -56,3 +56,29 @@ def search(term: str, count: int) -> List[SearchResult]:
     docs = s.query(dismax_query)[:count].execute()
 
     return [SearchResult.from_doc(d) for d in docs]
+
+def search_for_price_range(min_price: int, max_price: int, count: int) -> List[SearchResult]:
+    """ Search all products acording to price range. """
+
+    client = Elasticsearch()
+
+    # Elasticsearch 6 requires the content-type header to be set, and this is
+    # not included by default in the current version of elasticsearch-py
+    client.transport.connection_pool.connection.headers.update(HEADERS)
+
+    # TODO: seperate three cases, [min_price, ], [, max_price],
+    # and [min_price, max_price].
+    body = {
+        "query": {
+            "range": {
+                "price": {
+                    "gte": min_price,
+                    "lte": max_price
+                }
+            }
+        }
+    }
+    s = Search(using=client, index=INDEX_NAME, doc_type=DOC_TYPE).update_from_dict(body)
+    docs = s.query()[:count].execute()
+
+    return [SearchResult.from_doc(d) for d in docs]
